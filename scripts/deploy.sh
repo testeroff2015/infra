00f19f917d09f75a3e4ea884d30885460babdfa7
user <user@users-iMac.local>
2018-05-19 20:46:07 +0500

    Systemd integration

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,10 @@ source ~/.rvm/scripts/rvm
 git clone https://github.com/Artemmkin/reddit.git
 cd reddit && bundle install
 
-puma -d
+rvm alias create reddit ruby-2.4.1
+sed -i '1c#!\/bin\/bash'  ~/.rvm/wrappers/reddit/bundle
+#puma -d
+sudo cp ~/reddit/scripts/puma.service /etc/systemd/system
+sudo systemctl enable puma.service
+sudo systemctl start puma.service
 
-ps aux | grep puma
